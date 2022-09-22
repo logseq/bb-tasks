95e4fbdb7bbf1c720c6f8b58e3b3b96b3b487526
Gabriel Horner <gabriel@logseq.com>
2022-09-22 17:06:40 -0400

    Tweak ns-docs linter to handle multiple paths and from config

--- src/logseq/bb_tasks/lint/ns_docstrings.clj
+++ src/logseq/bb_tasks/lint/ns_docstrings.clj
@@ -4,10 +4,10 @@
   (:require [pod.borkdude.clj-kondo :as clj-kondo]
             [logseq.bb-tasks.util :as util]))
 
-(defn- missed-docstrings [paths {:keys [ignore-regex]}]
-  (let [analysis (:analysis (clj-kondo/run! {:lint paths
-                                             :config {:output {:analysis true}}}))
-        {:keys [namespace-definitions]} analysis
+(defn- get-undocumented-namespaces [paths {:keys [ignore-regex] :as config}]
+  (let [{{:keys [namespace-definitions]} :analysis}
+        (clj-kondo/run! {:lint paths
+                         :config {:output {:analysis true}}})
         ignore-var? (if ignore-regex
                       #(re-find (re-pattern ignore-regex) %)
                       (constantly false))
@@ -15,23 +15,29 @@
                                  (fn [{:keys [name doc no-doc lang]}]
                                    (and (not doc)
                                         (not no-doc)
-                                        (not= lang :clj)
+                                        ;; Most langs are weirdly nil
+                                        (contains? #{nil (:lang config)} lang)
                                         (not (ignore-var? (str name)))))
                                  namespace-definitions)]
-    (if (seq undocumented-namespaces)
-      (do
-        (println "\nThe following namespaces are undocumented:")
-        (run! println (map :name undocumented-namespaces))
-        (System/exit 1))
-      (println "\nAll namespaces are documented!"))))
+    undocumented-namespaces))
 
 (defn- read-config
   []
-  (:ns-docstrings (util/read-tasks-config)))
+  (merge
+   {:lang :cljs}
+   (:ns-docstrings (util/read-tasks-config))))
 
 (defn -main
   "Lint given classpath for namespaces missing docstrings."
   [& args]
-  (let [paths (or (first args) "src")
-        config (read-config)]
-    (missed-docstrings [paths] config)))
+  (let [config (read-config)
+        paths (or (seq args)
+                  (:paths config)
+                  ["src"])
+        namespaces (get-undocumented-namespaces paths config)]
+    (if (seq namespaces)
+      (do
+        (println "\nThe following namespaces are undocumented:")
+        (run! println (map :name namespaces))
+        (System/exit 1))
+      (println "\nAll namespaces are documented!"))))
