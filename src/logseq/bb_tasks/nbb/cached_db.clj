1d064cac01b26ae38c98ca140dcb257596d1e290
Gabriel Horner <gabriel@logseq.com>
2022-11-26 01:21:56 -0500

    Update cached-db to use incremental caching possible with
    https://github.com/logseq/logseq/pull/7363

--- src/logseq/bb_tasks/nbb/cached_db.clj
+++ src/logseq/bb_tasks/nbb/cached_db.clj
@@ -7,9 +7,11 @@
   "Writes a new cached db if any of the graph's files have changed"
   ;; :depends fn are weirdly pass their dependent's args so allow anything through
   [& _args]
-  (when-let [files (seq (fs/modified-since (fs/file "script/.transit-db-cache.json")
+  (when-let [files (seq (fs/modified-since (fs/file "script/.cached-db-transit.json")
                                            (fs/glob "." "{pages,journals,assets,logseq,whiteboards,draws}/*")))]
-    (println (format "Updating db since %s file(s) have changed!" (count files)))
-    (shell {:dir "script"} "yarn nbb-logseq -e"
+    (println (format "Updating db since %s file(s) changed!" (count files)))
+    (shell {:dir "script"} "yarn -s nbb-logseq -e"
            (str "(require '[logseq.bb-tasks.nbb.cached-db]) "
-                "(logseq.bb-tasks.nbb.cached-db/write-db \"..\")"))))
+                "(logseq.bb-tasks.nbb.cached-db/write-db \"..\" "
+                (pr-str (mapv #(str "../" (str %)) files))
+                " )"))))
--- src/logseq/bb_tasks/nbb/cached_db.cljs
+++ src/logseq/bb_tasks/nbb/cached_db.cljs
@@ -2,12 +2,14 @@
   "Handles nbb side of cached-db functionality. File path and cache implementation
 encapsulated in this ns"
   (:require [datascript.transit :as dt]
+            [datascript.core :as d]
             [logseq.graph-parser.cli :as gp-cli]
+            [logseq.graph-parser :as graph-parser]
             ["fs" :as fs]))
 
 (def ^:private cache-file
   "Cache file for storing transit db"
-  ".transit-db-cache.json")
+  ".cached-db-transit.json")
 
 (defn read-db
   "Reads db from cache file"
@@ -15,9 +17,28 @@ encapsulated in this ns"
   (dt/read-transit-str (fs/readFileSync cache-file)))
 
 (defn write-db
-  "Writes cache db file given the graph directory"
-  [dir]
-  (let [conn (:conn (gp-cli/parse-graph dir {:verbose false}))]
-    (println "Writing cache file" cache-file)
-    (fs/writeFileSync cache-file (dt/write-transit-str @conn))
-    nil))
+  "Writes cache db file given the graph directory. First time writing the cache
+  can take long for large graphs since all files are parsed. Subsequent writes
+  are quick as the cache only needs to update changed files."
+  [dir changed-files]
+  (if (fs/existsSync cache-file)
+    (let [old-conn (d/conn-from-db (read-db))
+          files (map
+                 #(hash-map :file/path % :file/content (str (fs/readFileSync %)))
+                 changed-files)
+          delete-blocks (fn [db file-page file-path uuid-blocks]
+                          (into (graph-parser/get-blocks-to-delete db file-page file-path uuid-blocks)
+                                ;; Delete page to allow for page properties to be redefined
+                                ;; Assumes no page rename for now
+                                [[:db.fn/retractEntity [:block/name (:block/name file-page)]]]))
+          conn (:conn (gp-cli/parse-graph dir
+                                          {:conn old-conn
+                                           :files files
+                                           :verbose true
+                                           :parse-file-options {:delete-blocks-fn delete-blocks}}))]
+      (println "Updating cache file" cache-file)
+      (fs/writeFileSync cache-file (dt/write-transit-str @conn)))
+    (let [conn (:conn (gp-cli/parse-graph dir {:verbose false}))]
+      (println "Writing cache file" cache-file)
+      (fs/writeFileSync cache-file (dt/write-transit-str @conn))))
+  nil)
